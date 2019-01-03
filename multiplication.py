c1d737cec46a1ee5b5cf98628379e9dae63c064a
leiperjack <leiperjack1997@gmail.com>
2019-01-03 14:25:26 +0000

    first commit

---
+++ multiplication.py
@@ -0,0 +1,3 @@
+def multiplication(a, b):
+    answer = a * b
+    return answer
---
+++ test_multiplication.py
@@ -0,0 +1,5 @@
+import pytest
+from multiplication import multiplication
+
+def test_multiply_a_and_b():
+    assert multiplication(5, 7) == 35
