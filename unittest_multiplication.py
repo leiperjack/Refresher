c83943078088242fbbdcffbde602c98db962caad
leiperjack <leiperjack1997@gmail.com>
2019-01-03 16:10:19 +0000

    fail test

--- unittest_multiplication.py
+++ unittest_multiplication.py
@@ -3,7 +3,7 @@ from multiplication import multiplication
 
 class myClass(unittest.TestCase):
     def test_multipication(self):
-        self.assertEqual(multiplication(6,3), (18))
+        self.assertEqual(multiplication(6,3), (17))
 
 if __name__ == "__main__":
     unittest.main()
